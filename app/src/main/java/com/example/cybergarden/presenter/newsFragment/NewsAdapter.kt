c6250ba2169dc06878edf21cc1eb12ccf5ba54f8
Victor Spirin <greatgomer3@gmail.com>
2021-12-10 22:58:33 +0300

    Refactor adapter

--- app/src/main/java/com/example/cybergarden/presenter/newsFragment/NewsAdapter.kt
+++ app/src/main/java/com/example/cybergarden/presenter/newsFragment/NewsAdapter.kt
@@ -1,40 +1,33 @@
 package com.example.cybergarden.presenter.newsFragment
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.cybergarden.R
+import com.example.cybergarden.databinding.NewsItemBinding
 
 class NewsAdapter(private val names: List<String>) :
     RecyclerView.Adapter<NewsAdapter.MyViewHolder>() {
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var title: TextView? = null
-        var description: TextView? = null
-        var date: TextView? = null
-        var image: ImageView? = null
-
-        init {
-            title = itemView.findViewById(R.id.title)
-            description = itemView.findViewById(R.id.description)
-            date = itemView.findViewById(R.id.date)
+    class MyViewHolder(private val binding: NewsItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(names: List<String>, position: Int) {
+            binding.apply {
+                title.text = names[position]
+                description.text = "кот"
+                date.text = "12.12.2012"
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView =
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.news_item, parent, false)
-        return MyViewHolder(itemView)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = NewsItemBinding.inflate(inflater, parent, false)
+
+        return MyViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.title?.text = names[position]
-        holder.description?.text = "кот"
-        holder.date?.text = "12.12.2012"
+        holder.onBind(names, position)
     }
 
     override fun getItemCount() = names.size
