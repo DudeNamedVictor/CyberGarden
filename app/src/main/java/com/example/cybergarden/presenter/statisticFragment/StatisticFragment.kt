1a615e6db32db1b0f6786e9fa1fb052ce7c1ea56
Victor Spirin <greatgomer3@gmail.com>
2021-12-10 17:01:51 +0300

    Add fragments

---
+++ app/src/main/java/com/example/cybergarden/presenter/statisticFragment/StatisticFragment.kt
@@ -0,0 +1,23 @@
+package com.example.cybergarden.presenter.statisticFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cybergarden.databinding.StatisticFragmentLauoutBinding
+
+class StatisticFragment : Fragment() {
+
+    private lateinit var binding: StatisticFragmentLauoutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = StatisticFragmentLauoutBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+}
